ade16b061e0c58fb247d95657725c5eca7c1eb3a
KietTuan <theonlytruth25012005@gmail.com>
2025-12-28 17:11:30 +0700

    Completed notification service

---
+++ backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/notification/controller/EmailController.java
@@ -0,0 +1,30 @@
+package com.theatermgnt.theatermgnt.notification.controller;
+
+import com.theatermgnt.theatermgnt.common.dto.response.ApiResponse;
+import com.theatermgnt.theatermgnt.notification.dto.request.SendEmailRequest;
+import com.theatermgnt.theatermgnt.notification.dto.response.EmailResponse;
+import com.theatermgnt.theatermgnt.notification.service.EmailService;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequestMapping("/notifications")
+public class EmailController {
+    EmailService emailService;
+
+    @PostMapping("/email/send")
+    ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request) {
+        return ApiResponse.<EmailResponse>builder()
+                .result(emailService.sendEmail(request))
+                .build();
+    }
+}
---
+++ backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/notification/enums/EmailType.java
@@ -0,0 +1,7 @@
+package com.theatermgnt.theatermgnt.notification.enums;
+
+public enum EmailType {
+    RESET_PASSWORD,
+    WELCOME_STAFF,
+    NOTIFICATION_EMAIL
+}
