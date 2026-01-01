fda33d1e94d0767804fa3c36958564de2cc340a2
KietTuan <theonlytruth25012005@gmail.com>
2026-01-01 15:49:42 +0700

    Add genres for MovieSimpleReponse

--- backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/movie/dto/response/MovieSimpleResponse.java
+++ backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/movie/dto/response/MovieSimpleResponse.java
@@ -1,6 +1,7 @@
 package com.theatermgnt.theatermgnt.movie.dto.response;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.theatermgnt.theatermgnt.common.enums.MovieStatus;
@@ -23,6 +24,8 @@ public class MovieSimpleResponse {
     String title;
     String posterUrl;
     String trailerUrl;
+    Set<MovieResponse.GenreInfo> genres;
+
     Integer durationMinutes;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
